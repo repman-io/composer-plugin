27805234165744e44e47a72a88395d0926b7b64c
Seveny Media <contact@sevenymedia.nl>
2020-11-12 14:58:59 +0100

    Implemented two methods required by `Composer\Plugin\PluginInterface` in `^2.0`

--- src/Repman.php
+++ src/Repman.php
@@ -79,4 +79,14 @@ final class Repman implements PluginInterface, EventSubscriberInterface
             }
         }
     }
+
+    public function deactivate(Composer $composer, IOInterface $io): void
+    {
+        $this->io->write(sprintf('Repman plugin (%s) deactivated', static::VERSION), true, IOInterface::VERBOSE);
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io): void
+    {
+        // TODO: Implement uninstall() method.
+    }
 }
